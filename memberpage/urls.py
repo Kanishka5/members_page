e22de9805023f00a53ff9a2ffba106a4eaab7474
kenny <kanishkan14@gmail.com>
2018-06-15 11:22:13 +0530

    urls

--- memberpage/urls.py
+++ memberpage/urls.py
@@ -14,13 +14,14 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import url
+from django.conf.urls import url,include
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^members/', include('members.urls')),
 ]
 
 
