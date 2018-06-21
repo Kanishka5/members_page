fd63da6b7ce50ac76ea664cdaa0db6339844bd19
kenny <kanishkan14@gmail.com>
2018-06-21 23:23:06 +0530

    changed default image

--- members/models.py
+++ members/models.py
@@ -4,7 +4,7 @@ class Member(models.Model):
     cell = models.CharField(max_length=50)
     name=models.CharField(max_length=100)
     post=models.CharField(max_length=100)
-    pics=models.ImageField(default='homepage/image/background.png')
+    pics=models.ImageField(default='background.jpg')
 
     def __str__(self):
         return self.name + '-' +self.cell
